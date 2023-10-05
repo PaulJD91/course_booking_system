6e6b6720539c95c5c8ea5e960150f6843f1de534
Ewelina Nicholson <e.guzikk@gmail.com>
2023-10-05 15:52:33 +0100

    Customer controller

---
+++ backend/src/main/java/com/codeclan/backend/repositories/CustomerRepository.java
@@ -0,0 +1,14 @@
+package com.codeclan.backend.repositories;
+
+import com.codeclan.backend.models.Course;
+import com.codeclan.backend.models.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends JpaRepository <Customer, Long> {
+    List<Customer>getAllCustomers(String name);
+
+    List<Customer> findByCourseId(Long courseId);
+}
+
