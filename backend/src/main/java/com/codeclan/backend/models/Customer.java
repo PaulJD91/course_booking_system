5c24507a66a06b90c1cf5a5e81102486292721cf
Ewelina Nicholson <e.guzikk@gmail.com>
2023-10-05 14:11:54 +0100

    Done customer model

--- backend/src/main/java/com/codeclan/backend/models/Customer.java
+++ backend/src/main/java/com/codeclan/backend/models/Customer.java
@@ -1,4 +1,78 @@
 package com.codeclan.backend.models;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "customers")
 
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "town")
+    private String town;
+
+    @Column(name = "dateOfBirth")
+    private LocalDate dateOfBirth;;
+
+    @OneToMany(mappedBy = "customer", fetch = FetchType.LAZY)
+    @JsonIgnoreProperties({"customer"})
+    private List<Booking> bookings;
+
+    public Customer(String name, String town, String dateOfBirth, List<Booking> bookings) {
+        this.name = name;
+        this.town = town;
+        this.dateOfBirth = LocalDate.parse(dateOfBirth);
+        this.bookings = bookings;
+    }
+    public Customer() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = LocalDate.parse(dateOfBirth);
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
+
 }
